1bcce82ce0735cb3f14a2cad942b1e654eddd7b0
dastinia <690205072@qq.com>
2022-08-13 10:39:38 +0800

    第一次提交，误删除了style.css文件

---
+++ js/Identify.js
@@ -0,0 +1,7 @@
+function userIdentify(username) {
+    let usernameInput = document.getElementById("userIdentify");
+    usernameInput.onblur = function() {
+        let username = usernameInput.nodeValue.trim();
+        
+    }
+}
